09b2765c74ce32f0229d3e89bb58751a6a5d1ae7
HaroonVankra <thamivankra@gmail.com>
2014-07-03 14:33:42 -0700

    hroon did it again

--- config.php
+++ config.php
@@ -1,6 +1,13 @@
-<?php
-	$host = "localhost";
+<!--?php
+	$host = "mysql.itvarsity.co.za:3307";
 	$username = "root";
 	$password = "";
 	$database = "notesplus";
+?-->
+
+<?php
+	$host = "localhost";
+	$username = "kat17_itv140037";
+	$password = "tBohO2s+@H3M";
+	$database = "kat17_itv_student";
 ?>
---
+++ install.php
@@ -0,0 +1,54 @@
+<?php
+    include 'config.php';
+    include 'functions.php';
+	
+	
+     $connection = openDB();
+	 $query = "DROP TABLE IF EXISTS bootcamp2014_01_users,bootcamp2014_01_notes";
+	  $results = mysql_query($query)
+				or die("query failed: ".mysql_error());
+	 
+    $query ="CREATE TABLE IF NOT EXISTS `notesplus`.`bootcamp2014_01_users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `firstname` VARCHAR(100) NULL,
+  `lastname` VARCHAR(100) NULL,
+  `username` VARCHAR(100) NULL,
+  `password` VARCHAR(200) NULL,
+  `email` VARCHAR(50) NULL,
+  `gender` VARCHAR(1) NULL,
+  `active` TINYINT(1) NULL,
+  `activaitionDate` DATETIME NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB" ;
+    
+    $results = mysql_query($query)
+				or die("query failed: ".mysql_error());
+				
+	
+	
+	$query	= "CREATE TABLE IF NOT EXISTS `notesplus`.`bootcamp2014_01_notes` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `tittle` VARCHAR(200) NULL,
+  `noteText` TEXT NULL,
+  `date` DATETIME NULL,
+  `users_id INT` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_notes_users_idx` (`users_id INT` ASC),
+  CONSTRAINT `fk_notes_users`
+    FOREIGN KEY (`users_id INT`)
+    REFERENCES `notesplus`.`bootcamp2014_01_users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+";
+	
+	
+		$results = mysql_query($query)
+				or die("query failed: ".mysql_error());
+	
+	
+				
+			
+				
+		 closeDB($connection);
+?>

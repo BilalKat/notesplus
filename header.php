d408d2dc49d7ef68024865349b5f2e28d1ec3ff9
sahalmotala <sahalmotala4@gmail.com>
2014-07-16 08:55:24 +0200

    sahl

--- header.php
+++ header.php
@@ -1,3 +1,11 @@
+<?php
+        session_start();
+			if(!isset($_SESSION['isLoggedIn'])){
+				header("Location:localhost/notesplus/loginform.php");
+			}
+       
+?>
+
 <!doctype html>
 <html lang="en-us">
 <head>
